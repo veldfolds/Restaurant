825e39399e5822497a1c0822bac1858116dc2823
Adrian <Kubokaadrian@outlook.com>
2023-01-15 18:56:23 +0300

    MODEL & VIEWMODEL Added menu page view model and also the menu item model

---
+++ Models/RestaurantMenuItem.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiApp2.Models;
+
+class RestaurantMenuItem
+{
+    public string Name { get; set; }
+
+    public string CommonName { get; set; }
+
+    public double Price { get; set; }
+
+    public double Ratings { get; set; }
+
+    public string Carbohydrate { get; set; }
+
+    public string Proteins { get; set; }
+
+    public string Fibre { get; set; }
+
+    public double Offer { get; set; }
+
+    public int Kcal { get; set; }
+
+
+}
---
+++ ViewModels/MenuPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.ObjectModel;
+
+namespace MauiApp2.ViewModels;
+
+class MenuPageViewModel
+{
+    public ObservableCollection<RestaurantMenuItem> MenuItems { get; set; }
+
+	pro
+
+	public MenuPageViewModel()
+	{
+
+	}
+}
