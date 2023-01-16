4f4e67f62527e35a742df2b98f5c52dfb630911c
Adrian <Kubokaadrian@outlook.com>
2023-01-16 08:17:30 +0300

    MENU ITEMS SERVICE Added a menu items service to retrieve a list of menu items and also did some refactoring on the restaurant menu items model.

--- Models/RestaurantMenuItem.cs
+++ Models/RestaurantMenuItem.cs
@@ -2,23 +2,22 @@
 
 class RestaurantMenuItem
 {
-    public string Name { get; set; }
-
     public string CommonName { get; set; }
 
-    public double Price { get; set; }
+    public string Name { get; set; }
 
     public double Ratings { get; set; }
-
-    public string Carbohydrate { get; set; }
+    
+    public int Kcal { get; set; }
 
     public string Proteins { get; set; }
 
+    public string Carbohydrate { get; set; }
+
     public string Fibre { get; set; }
 
-    public double Offer { get; set; }
+    public double Discount { get; set; }
 
-    public int Kcal { get; set; }
-
+    public double Price { get; set; }
 
 }
---
+++ Services/MenuItemsService.cs
@@ -0,0 +1,51 @@
+﻿using MauiApp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp2.Services
+{
+    internal static class MenuItemsService
+    {
+        public static List<RestaurantMenuItem> MenuItems => new()
+        {
+           new() 
+           { 
+               CommonName = "White Chicken",
+               Name = "Spicy Chicken",
+               Ratings = 4.8,
+               Discount = 2.1,
+               Kcal = 510,
+               Proteins = "16,2",
+               Carbohydrate = "13,9",
+               Fibre = "8,6",
+               Price = 18.99
+           },
+           new()
+           {
+               CommonName = "Original Italian",
+               Name = "Buffalo mozzarella",
+               Ratings = 3.9,
+               Discount = 0,
+               Kcal = 436,
+               Proteins = "11.8",
+               Carbohydrate = "21,3",
+               Fibre = "6,4",
+               Price = 12.58
+           },new()
+           {
+               CommonName = "Meat Mexican",
+               Name = "Ground Beef Tacos",
+               Ratings = 4.2,
+               Discount = 0,
+               Kcal = 312,
+               Proteins = "10,8",
+               Carbohydrate = "18,9",
+               Fibre = "7,2",
+               Price = 10.99
+           },
+        };
+    }
+}
