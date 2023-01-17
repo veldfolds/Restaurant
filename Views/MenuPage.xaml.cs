e281f745bc1f0460771fad1f3fb9dba74a9a4b08
Adrian <Kubokaadrian@outlook.com>
2023-01-17 12:38:14 +0300

    MENU PAGE MENU ITEMS Connected collection view to view model and items are displaying.

--- Views/MenuPage.xaml.cs
+++ Views/MenuPage.xaml.cs
@@ -1,9 +1,14 @@
+using MauiApp2.ViewModels;
+
 namespace MauiApp2.Views;
 
 public partial class MenuPage : ContentPage
 {
 	public MenuPage()
 	{
+		BindingContext = new MenuPageViewModel();
+
 		InitializeComponent();
+
 	}
 }
